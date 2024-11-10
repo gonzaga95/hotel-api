5f221f1e789dcf6bce5ee5cb19e861bb15ddb63e
gonzaga95 <gonzagas.dudu@gmail.com>
2024-11-10 00:40:44 -0400

    add arquivos ignorados

---
+++ target/classes/data.sql
@@ -0,0 +1,10 @@
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
+INSERT into reserva_hotel(NOME_CLIENTE, NUMERO_RESERVA) VALUES ('Carlos Eduardo Gonzaguinha', 12345);
